70ddd1d75d3d4347abe732b0ad3b85df854e3719
James.Jian <james.jian@agersens.com>
2021-05-02 01:05:06 +1000

    MQTT
    Test MQTT

---
+++ secrets.py
@@ -0,0 +1,14 @@
+# This file is where you keep secret settings, passwords, and tokens!
+# If you put them in the code you risk committing that info or sharing it
+
+secrets = {
+    'ssid' : 'XXX',
+    'password' : 'XXX',
+    'aio_username' : 'jamesjian000',
+    'aio_key' : 'XXXXX',
+    'timezone' : "America/New_York", # http://worldtimeapi.org/timezones
+    'broker' : 'io.adafruit.com',
+    'port'   : 1883,
+    'user' : 'XXX',
+    'pass' : 'XXX'
+    }
